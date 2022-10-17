093a2ef01bf648feea3d63a08b0bc96ca3632d38
Your Name <you@example.com>
2022-10-17 14:40:56 +0530

    first Update

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:registration_ui/utils/appRoutes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
@@ -24,141 +25,145 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 30,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Log IN',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      child: TextFormField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Email',
+                SizedBox(
+                  height: 30,
+                ),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: TextFormField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
+                          validator: (val) {
+                            if (val!.isNotEmpty) {
+                              isEmailValid = true;
+                              setState(() {});
+                            } else {
+                              return "Enter valid Email";
+                            }
+                          },
                         ),
-                        validator: (val) {
-                          if (val!.isNotEmpty) {
-                            isEmailValid = true;
-                            setState(() {});
-                          } else {
-                            return "Enter valid Email";
-                          }
-                        },
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      child: TextFormField(
-                        obscureText: true,
-                        controller: passwordController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Password',
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: TextFormField(
+                          obscureText: true,
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                          validator: (val) {
+                            if (val!.isNotEmpty) {
+                              isPasswordValid = true;
+                              setState(() {});
+                            } else {
+                              return "Enter valid Email";
+                            }
+                          },
                         ),
-                        validator: (val) {
-                          if (val!.isNotEmpty) {
-                            isPasswordValid = true;
-                            setState(() {});
-                          } else {
-                            return "Enter valid Email";
-                          }
-                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: checkBoxValue,
+                      onChanged: (val) {
+                        setState(() {
+                          checkBoxValue = val!;
+                        });
+                      },
+                    ),
+                    Text("Remember me"),
+                    Spacer(),
+                    TextButton(
+                      onPressed: () {
+                        //forgot password screen
+                      },
+                      child: const Text(
+                        'Forgot Password',
                       ),
                     ),
                   ],
                 ),
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: checkBoxValue,
-                    onChanged: (val) {
-                      setState(() {
-                        checkBoxValue = val!;
-                      });
-                    },
-                  ),
-                  Text("Remember me"),
-                  Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      //forgot password screen
-                    },
-                    child: const Text(
-                      'Forgot Password',
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 50,
-                width: 200,
-                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                  ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-
-                    String? data = prefs.getString("signUp");
-
-                    log("${data}", name: "Data");
-
-                    // String? email = prefs.getString("email");
-                    // String? password = prefs.getString("password");
-
-                    print(emailController.text);
-                    print(passwordController.text);
-                  },
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Row(
-                children: [
-                  const Text('Does not have account?'),
-                  TextButton(
-                    child: const Text(
-                      'Sign up',
-                      style: TextStyle(fontSize: 20),
+                Container(
+                  height: 50,
+                  width: 200,
+                  padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: StadiumBorder(),
                     ),
-                    onPressed: () {},
-                  )
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-            ],
+                    child: const Text(
+                      'Login',
+                      style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    onPressed: () async {
+                      SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+
+                      String? data = prefs.getString("Login");
+
+
+                      // String? email = prefs.getString("email");
+                      // String? password = prefs.getString("password");
+
+                      print(emailController.text);
+                      print(passwordController.text);
+
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                          AppRoutes().homePage, (route) => false);
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Row(
+                  children: [
+                    const Text('Does not have account?'),
+                    TextButton(
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              ],
+            ),
           ),
         ),
       ),
